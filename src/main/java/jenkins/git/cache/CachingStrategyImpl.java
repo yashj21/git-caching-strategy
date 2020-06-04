5b7978894db8062b46f4a905b0fefff6fa14c49e
yashj21 <yashjain96@hotmail.com>
2020-06-04 14:54:05 -0700

    Resolve root directory

--- src/main/java/jenkins/git/cache/CachingStrategyImpl.java
+++ src/main/java/jenkins/git/cache/CachingStrategyImpl.java
@@ -11,10 +11,12 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.jenkinsci.plugins.gitclient.CloneCommand;
+import org.jenkinsci.plugins.gitclient.FetchCommand;
 import org.jenkinsci.plugins.gitclient.Git;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.AbortException;
 import hudson.EnvVars;
@@ -33,12 +35,13 @@ import hudson.plugins.git.extensions.impl.SparseCheckoutPath;
 
 public class CachingStrategyImpl extends CachingStrategy {
 
-	@NonNull //this can be null::create a custom workspace location
-    private String url;
+//	@NonNull //this can be null::create a custom workspace location
+//    private String url;
 	private static HashMap<String,ReentrantLock> agentCacheLocks = new HashMap<>();
 	@DataBoundConstructor
 	public CachingStrategyImpl(String url) {
-	        this.url = url;
+	        super(url);
+	        //setRepoWorkspace(url);
 	    }
 	public void setUrl(String url) {
         this.url = url;
@@ -46,46 +49,25 @@ public class CachingStrategyImpl extends CachingStrategy {
 	public String getUrl() {
 		return url;
 	}
-	public void cacheImpl(GitSCM scm, Run<?, ?> build, GitClient gitClient, TaskListener listener) {
-		List<RemoteConfig> repos = null;
-		try {
-		repos = scm.getParamExpandedRepos(build, listener);
-		if (repos == null || repos.isEmpty())    return;
-		RemoteConfig rc = repos.get(0);
-		String cacheEntry = createCacheEntry(rc.getURIs());
-        FilePath cacheDir = new FilePath(new File(url));
-		if(!cacheDir.isDirectory()) {
-			cacheDir.mkdirs();
-		}
-		Lock cacheLock = getCacheLock(cacheEntry);
-		cacheLock.lock();
-		try {
-			EnvVars env =build.getEnvironment(listener);
-			GitClient git = scm.createClient(listener, env, build, cacheDir);
-			if(git.hasGitRepo()) {
-				  if (repos.size() == 1)
-		               listener.getLogger().println("Fetching changes from the cache Git repository");
-		            else
-		            listener.getLogger().println("Fetching changes from {0} cache Git repositories " +repos.size());
-			}
-			listener.getLogger().println("Cloning the remote Git repository in cache dir");
-
-            try {
-                CloneCommand cmd = git.clone_().url(rc.getURIs().get(0).toPrivateString()).repositoryName(rc.getName());
+	@Override
+	public void cacheImpl(GitSCM scm, Run<?, ?> build, GitClient gitClient, TaskListener listener,List<RemoteConfig> repos) {
+			if (repos == null || repos.isEmpty())    return;
+			RemoteConfig rc = repos.get(0);
+			String cacheEntry = createCacheEntry(rc.getURIs());
+			Lock cacheLock = getCacheLock(cacheEntry);
+			cacheLock.lock();
+			try {
+                CloneCommand cmd = gitClient.clone_().url(rc.getURIs().get(0).toPrivateString()).repositoryName(rc.getName());
                 for (GitSCMExtension ext : scm.getExtensions()) {
-                	if(!ext.getClass().equals(SparseCheckoutPath.class))
-                		ext.decorateCloneCommand(scm, build, git, listener, cmd);
+                	ext.decorateCloneCommand(scm, build, gitClient, listener, cmd);
                 }
                 cmd.execute();
-            } catch (GitException ex) {
+            } catch (GitException | InterruptedException | IOException ex) {
                 ex.printStackTrace(listener.error("Error cloning remote repo '" + rc.getName() + "'"));
-                throw new AbortException("Error cloning remote repo '" + rc.getName() + "'");
-            }
-        }catch (Exception e) {
-		}
-		}catch(IOException | InterruptedException e) {
-			//continue for now
-		}
+                //throw new AbortException("Error cloning remote repo '" + rc.getName() + "'");
+            }finally {
+    			cacheLock.unlock();
+    		}
 		//create cache entry from repo url;
 		//retrieve the lock
 		//get the cache dir from the hash 
@@ -95,16 +77,28 @@ public class CachingStrategyImpl extends CachingStrategy {
 		//compute the difference with remote repo name
 		//use git CLI getHeadRev
 		//
-	}
-
-	private String createCacheEntry(List<URIish> urIs) {
-		String remotename = "";
-		if(urIs!=null && urIs.size()>0) {
-			remotename = urIs.get(0).toPrivateString();
-		}
-		return "git-" + Util.getDigestOf(remotename);
-	}
+	}   
 	
+
+			  
+//	private String createCacheEntry(List<URIish> urIs) {
+//		String remotename = "";
+//		if(urIs!=null && urIs.size()>0) {
+//			remotename = urIs.get(0).toPrivateString();
+//		}
+//		return "git-" + Util.getDigestOf(remotename);
+//	}
+	@Override
+	public void fetchChanges(GitSCM scm, Run<?, ?> build, GitClient git, TaskListener listener,List<RemoteConfig> repos) {
+		  for (RemoteConfig remoteRepository : repos) {
+	                try {
+						fetchFrom(scm,git, build, listener, remoteRepository);
+					} catch (InterruptedException | IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		  }
+	}
 	private Lock getCacheLock(String cacheEntry) {
 		  Lock cacheLock;
 	        while (null == (cacheLock = agentCacheLocks.get(cacheEntry))) {
